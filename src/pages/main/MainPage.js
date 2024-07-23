bc352285954c0ff84455bc337d31483d42ff8909
Taeyoung20230727 <qksrm5546@gmail.com>
2024-07-23 17:53:23 +0900

    login

---
+++ src/pages/main/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/main.scss';
+
+const MainPage = () => {
+  return (
+    <div className="Main">
+      <h2>메인 대시보드</h2>
+      <img src="../images/pikachu.gif" alt='피카츄'></img>
+    </div>
+  )
+}
+export default MainPage
--- src/router/root.js
+++ src/router/root.js
@@ -4,6 +4,15 @@ import MainPage from "../pages/main/MainPage";
 import LoginPage from "../pages/user/LoginPage";
 import ListPage from "../pages/pokemons/ListPage";
 import DashboardPage from "../pages/admin/DashboardPage";
+import Terms from "../pages/user/Terms";
+import Register from "../pages/user/Register";
+import FindId from "../pages/user/FindId";
+import FindPass from "../pages/user/FindPass";
+
+import UserlistPage from "../pages/admin/UserListPage";
+
+import ChatRoom from "../components/chat/ChatRoom";
+import ChatRoomList from "../components/chat/ChatRoomList";
 
 
 //router 생성
@@ -13,13 +22,23 @@ const root = createBrowserRouter([
     
     // user
     {path: "/user/login", element:<LoginPage />}, // 로그인
-
+    {path: "/user/Terms", element:<Terms />}, // 약관
+    {path: "/user/register", element:<Register />}, // 회원가입
+    {path: "/user/FindId", element:<FindId />}, // 아이디찾기
+    {path: "/user/FindPass", element:<FindPass />}, // 비번찾기
 
     // pokemons
     {path: "/pokemons/list", element:<ListPage />}, // 포켓몬 목록
 
     // admin
     {path: "/admin/dashboard", element:<DashboardPage />}, // 관리자 대시보드
+
+    {path: "/admin/userlist", element:<UserlistPage/>}, //관리자 - 회원관리
+
+
+    // chat
+    { path: "/chatrooms", element: <ChatRoomList /> },
+    { path: "/chatroom/:chatNo", element: <ChatRoom /> },
 
 ]);
 
